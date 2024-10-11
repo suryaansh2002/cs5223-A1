364518db67c5ed5ece1c8510803560a81d20db28
sneha <30837089+itmesneha@users.noreply.github.com>
2024-10-11 18:42:19 +0800

    Update Position.java

--- Position.java
+++ Position.java
@@ -51,12 +51,4 @@ public class Position implements Serializable{
         if (x != null ? !x.equals(position.x) : position.x != null) return false;
         return y != null ? y.equals(position.y) : position.y == null;
     }
-
-    // //  Generates a hash code for this Position object based on its x and y coordinates.
-    // @Override
-    // public int hashCode() {
-    //     int result = x != null ? x.hashCode() : 0;
-    //     result = 31 * result + (y != null ? y.hashCode() : 0);
-    //     return result;
-    // }
 }
